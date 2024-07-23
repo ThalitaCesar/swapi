680a0f98ecbbb07a947e70f1d085233e29dec32e
ThalitaCesar <thalita.walleska@gmail.com>
2024-07-23 17:48:43 -0300

    ajustes state

--- src/pages/Character/Character.tsx
+++ src/pages/Character/Character.tsx
@@ -1,40 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import CardCharacterInformation from './components/CardCharacterInformation/CardCharacterInformation';
-import bgImage from '../../assets/bg4.jpg'; 
-import './Character.scss'; 
+import bgImage from '../../assets/bg4.jpg';
+import './Character.scss';
 import Navbar from '../../components/Navbar/Navbar';
 import Button from '../../components/Button/Button';
 
 const Character: React.FC = () => {
-  const { state } = useLocation<{ character: any }>(); 
+  const { state } = useLocation<{ character: any }>();
   const navigate = useNavigate();
 
+  const [character, setCharacter] = useState<any>(null);
+
+  useEffect(() => {
+    if (state && state.character) {
+      setCharacter(state.character);
+    }
+  }, [state]);
+
   const handleVoltar = () => {
-    navigate(-1); 
+    navigate(-1);
   };
 
-  if (!state || !state.character) {
-    return <div>Personagem não encontrado.</div>;
+  // Verifica se o character ainda é null, então não renderiza o CardCharacterInformation
+  if (!character) {
+    return <div>Carregando...</div>;
   }
 
-  const { character } = state;
-
-  console.log(character)
+  console.log(character);
 
   return (
-
     <div className="character-container">
-    <div className="background-image" style={{ backgroundImage: `url(${bgImage})` }}></div>
-    <div className="gradient"></div>
-    <div className="content">
-    <Navbar/>
-    <CardCharacterInformation character={character} />
-    <div className="button-container">
+      <div className="background-image" style={{ backgroundImage: `url(${bgImage})` }}></div>
+      <div className="gradient"></div>
+      <div className="content">
+        <Navbar />
+        {/* Verifica se character é null antes de passá-lo para o CardCharacterInformation */}
+        {character && <CardCharacterInformation character={character} />}
+        <div className="button-container">
           <Button text="Voltar" onClick={handleVoltar} />
         </div>
+      </div>
     </div>
-  </div>
   );
 };
 
